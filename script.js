ba1647b01be981e1860c79bfd7869831349bbd24
icekingass <113920959+icekingass@users.noreply.github.com>
2023-06-03 15:58:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
+// JavaScript content
+
 var FAQ = document.getElementById("FAQ");
 var Pagrindinis = document.getElementById("Pagrindinis");
 var Apie = document.getElementById("Apie");
 var Kontaktai = document.getElementById("Kontaktai");
+var MenuBtn = document.getElementById("MenuBtn");
+var SubMenu = document.getElementsByClassName("sub-menu")[0];
+var sidebar = document.getElementById("sidebar");
 
 FAQ.addEventListener("click", function () {
-  // Redirect or scroll to the section
   window.location.href = "#";
 });
 
 Pagrindinis.addEventListener("click", function () {
-  // Redirect or scroll to the section
   window.location.href = "#";
 });
 
 Apie.addEventListener("click", function () {
-  // Redirect or scroll to the section
   window.location.href = "#";
 });
 
 Kontaktai.addEventListener("click", function () {
-  // Redirect or scroll to the section
   window.location.href = "#";
+});
+
+MenuBtn.addEventListener("click", function () {
+  sidebar.classList.toggle("open");
+  
+  if (sidebar.classList.contains("open")) {
+    MenuBtn.style.transform = "translateX(400px)";
+  } else {
+    MenuBtn.style.transform = "translateX(0)";
+  }
 });
